babd519b6100a8495073cdb5fab389b45ca2b62d
Roman Borisov <rborisoff@gmail.com>
2016-11-24 23:29:01 +0300

    mainloop

--- main.c
+++ main.c
@@ -15,23 +15,71 @@
 
 #include "memory_utils.h"
 
-static int speedsaved = 0;
+#define _WAIT_GPS_ 2
+#define _MIN_MOVE_ 1
+#define _TIMEOUT_ 1
+
+static void fix(struct gps_data_t *gpsdata)
+{
+    double move;
+    static double moved = 0;
+    static double time_saved = 0;
+    static double lat_saved = 0, lon_saved = 0;
+    static bool first = true;
+    int speedkph;
+    char speedstr[4] = "\0";
+    char movedstr[6] = "\0";
+    static int speed_saved = 0;
+
+    if ((gpsdata->fix.time == time_saved) || gpsdata->fix.mode < MODE_2D)
+        return;
+
+    if (!first) {
+        move = earth_distance(gpsdata->fix.latitude, gpsdata->fix.longitude,
+                lat_saved, lon_saved);
+        if (move < _MIN_MOVE_)
+            return;
+        moved += move;
+        snprintf(movedstr, 5, "%f", moved/1000);
+        send_to_server("[moved]", movedstr);
+    }
+
+    if (first) {
+        first = false;
+    }
+
+    lat_saved = gpsdata->fix.latitude;
+    lon_saved = gpsdata->fix.longitude;
+    time_saved = gpsdata->fix.time;
+
+    speedkph = round(gpsdata->fix.speed*3.6);
+
+    if ((speedkph >= 0) && 
+            (speedkph < 300) && 
+            (speedkph != speed_saved)) {
+        sprintf(speedstr, "%d", speedkph);
+        send_to_server("[speed]", speedstr);
+        speed_saved = speedkph;
+    }
+}
 
 int main()
 {
     int rc;
-    struct timeval tv;
-    int speedkph;
-    char speedstr[3];
-
+//    struct timeval tv;
     struct gps_data_t gps_data;
-    if ((rc = gps_open("localhost", "2947", &gps_data)) == -1) {
+
+    while ((rc = gps_open("localhost", "2947", &gps_data)) == -1) {
         syslog(LOG_ERR, "%s: code: %d, reason: %s\n", __func__, rc, 
                 gps_errstr(rc));
-        return EXIT_FAILURE;
+        sleep(_WAIT_GPS_);
     }
 
     gps_stream(&gps_data, WATCH_ENABLE | WATCH_JSON, NULL);
+#if 1
+    gps_mainloop(&gps_data, 5000000, fix);
+    send_to_server("[gps]", "timeout");
+#else
     while (1) {
         /* wait for 2 seconds to receive data */
         if (gps_waiting (&gps_data, 2000000)) {
@@ -68,7 +116,7 @@ int main()
 
 //        sleep(1);
     }
-
+#endif
     /* When you are done... */
     gps_stream(&gps_data, WATCH_DISABLE, NULL);
     gps_close (&gps_data);
